04840f109d7dd2621f682ce98bad85c297b8bf4c
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2024-12-20 18:03:04 +0100

    Ristrutturato codice

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 sudo echo -e "Moving shell scripts into /usr/local/bin"
-sudo cp ./scripts/pmm-* /usr/local/bin
+sudo cp ./scripts/*pmm /usr/local/bin
+ls --color /usr/local/bin/*pmm*
+echo -e "Done!"
--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sudo echo -e "Removing pmm shell scripts from /usr/local/bin"
-sudo rm /usr/local/bin/pmm-apt /usr/local/bin/pmm-flat /usr/local/bin/pmm-fu
+sudo rm -v /usr/local/bin/aptpmm /usr/local/bin/flatpmm /usr/local/bin/fupmm /usr/local/bin/pmm
