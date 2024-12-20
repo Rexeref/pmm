89b2a0cafb9107347ea0e59990a82c40ad3e4133
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2024-12-20 13:52:32 +0100

    Reorganized files

---
+++ install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo echo -e "Moving shell scripts into /usr/local/bin"
+sudo cp ./scripts/pmm-* /usr/local/bin
---
+++ remove.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo echo -e "Removing pmm shell scripts from /usr/local/bin"
+sudo rm /usr/local/bin/pmm-apt /usr/local/bin/pmm-flat /usr/local/bin/pmm-fu
