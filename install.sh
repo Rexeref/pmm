fe1ff4c5f659ec90145df82852e2cbc742162057
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2025-04-01 20:45:15 +0200

    Changed pm from pacman to yay in fupmm & changed way borders are created

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+sudo ./remove.sh
 sudo echo -e "Moving shell scripts into /usr/local/bin"
 sudo cp ./scripts/*pmm /usr/local/bin
 ls --color /usr/local/bin/*pmm*
--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sudo echo -e "Removing pmm shell scripts from /usr/local/bin"
-sudo rm -v /usr/local/bin/aptpmm /usr/local/bin/flatpmm /usr/local/bin/fupmm /usr/local/bin/pacpmm /usr/local/bin/pmm
+sudo rm -v /usr/local/bin/aptpmm /usr/local/bin/flatpmm /usr/local/bin/fupmm /usr/local/bin/pacpmm /usr/local/bin/pmm /usr/local/bin/borderGen-pmm
