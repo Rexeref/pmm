997ff4bc8bbcc7a0be5caec7e9fb26655c7c0e67
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2025-04-01 21:02:02 +0200

    Added fzf install check

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-sudo ./remove.sh
-sudo echo -e "Moving shell scripts into /usr/local/bin"
-sudo cp ./scripts/*pmm /usr/local/bin
-ls --color /usr/local/bin/*pmm*
-echo -e "Done!"
+if ! command -v fzf 2>&1 >/dev/null
+then
+	echo -e "Fuzzy Finder (fzf) was not found, please install it!"
+else
+	sudo ./remove.sh
+	sudo echo -e "Moving shell scripts into /usr/local/bin"
+	sudo cp ./scripts/*pmm /usr/local/bin
+	ls --color /usr/local/bin/*pmm*
+	echo -e "Done!"
+fi
