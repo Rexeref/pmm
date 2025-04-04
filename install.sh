607f5aa61ff73c65551bd29635cb94449683b2fc
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2025-04-04 17:05:45 +0200

    Removed root check in fupmm

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ if ! command -v fzf 2>&1 >/dev/null
 then
 	echo -e "Fuzzy Finder (fzf) was not found, please install it!"
 else
-	sudo ./remove.sh
 	sudo echo -e "Moving shell scripts into /usr/local/bin"
 	sudo cp ./scripts/*pmm /usr/local/bin
 	ls --color /usr/local/bin/*pmm*
